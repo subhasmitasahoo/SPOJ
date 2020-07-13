df726c5aa78ac6b8ddf61e4db4658ab24a0eca39
Subhasmita Sahoo <subhasmitasahoo.247@gmail.com>
2020-07-13 20:24:29 +0530

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,10 @@
+#Problem link: https://www.spoj.com/problems/TEST/
+#Time complexity: O(1)
+#Space complexity: O(1)
+
+ip = True
+while True:
+    n = int(input())
+    if n==42:
+        break
+    print(n)
